38dcb7f514a837eae15a6e1d61fa96e027f9635c
Ricardo <perfecte1@hotmail.com>
2025-10-06 20:46:28 -0300

    segundo commit

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const { engine } = require("express-handlebars");
+const patch = require('path')
 
 const PORT = 3000;
 const app = express();
 
 app.engine('handlebars', engine({defaultLayout: 'main', extname: '.handlebars'}))
 app.set('view engine', 'handlebars')
+
+app.use(express.static(patch.join(__dirname, "css/")))
+app.use(express.static(patch.join(__dirname, "js/")))
 
 
 app.get('/', (req, res) => {
